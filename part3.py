636103c91b8850daaa52b66f2d35a49217f28145
CKline22 <CKline.1022@gmail.com>
2022-07-11 20:38:25 -0400

    finished part 3

--- part3.py
+++ part3.py
@@ -22,4 +22,30 @@ def recursive_factorial(i):
 print(recursive_factorial(5))
 print(recursive_factorial(9))    
 
+def recursive_deduplicate(my_str, i=0):
+    if len(my_str) <= 1 or i == len(my_str)-1:
+        return my_str
+    else:
+        if my_str[i] == my_str[i+1]:
+            return
+        recursive_deduplicate(my_str[0:i]+my_str[i+1:], i)
+        else:
+            return recursive_deduplicate(my_str, i+1)                  
+print(recursive_deduplicate("aaaa"))
+print(recursive_deduplicate("aaba"))
+print(recursive_deduplicate("apple"))
+print(recursive_deduplicate(""))                    
+
+def recursive_reverse(str, i=0):
+    if len(str)==0:
+        return ""
+    elif i == len(str)-1:
+        return str[0]
+    else:
+        return str[-1-i] + recursive_reverse(str, i+1)
+
+print(recursive_reverse("aaaa"))
+print(recursive_reverse("aaba"))
+print(recursive_reverse("apple"))
+print(recursive_reverse(""))               
 
