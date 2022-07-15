7462faccbb7cd99fe6773c4cd8e10e3718923bdf
CKline22 <CKline.1022@gmail.com>
2022-07-14 20:26:23 -0400

    finished

--- part4.py
+++ part4.py
@@ -14,4 +14,46 @@ def mult_list(list):
     return product
 
 print(mult_list([2,4,6]))
-print(mult_list([25,50]))                
+print(mult_list([25,50]))     
+
+def rev_string(str):
+    return str[::-1]
+
+print(rev_string(""))
+print(rev_string("meat"))
+
+
+def num_within(x, a, b):
+    return x in range(a, b+1)
+
+print(num_within(12,24,36))
+print(num_within(12,15,12))
+
+
+triangle = [[1],[1,1]]
+def pascal(i):
+    if i < 1:
+        print("not a number of rows")
+    elif i == 1:
+        print(triangle[0])
+    else:
+        row_num = 2
+        while len(triangle) < i:
+            row = []
+            row_previous = triangle[row_num - 1]
+            length = len(row_previous) + 1
+            for e in range(length):
+                if e == 0:
+                    row.append(1)  
+                elif e > 0 and e < length - 1:
+                    row.append(triangle[row_num-1][e-1]+ triangle[row_num - 1][e])
+                else:
+                    row.append(1)
+            triangle.append(row)
+            row_num += 1
+
+        for row in triangle:
+            print(row)
+
+pascal(6)
+pascal(10)
